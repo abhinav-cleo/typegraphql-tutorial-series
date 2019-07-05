a3cc126a797f9b06328b2daec5f66224c76d3d1d
Gupta <agupta@Cleoadmins-MacBook-Pro.local>
2019-07-05 15:58:56 +0530

    Added

--- Server/src/modules/user/UserHelpers.ts
+++ Server/src/modules/user/UserHelpers.ts
@@ -1,19 +1,8 @@
-import {UserValidationInput} from "./UserValidationInput";
-import {isEqual} from "apollo-utilities/lib/util/isEqual";
-import {UserOutput} from "./UserOutput";
 import {User} from "../../entity/User";
 import * as jwt from "jsonwebtoken";
 
 export class UserHelpers {
     public static readonly JWT_SECRET = "@QEGTUI";
-    public static isUserValid(userCredFromDB: UserValidationInput, userCredFromRequest): Boolean{
-        return isEqual(userCredFromDB,userCredFromRequest);
-    }
-
-    public static toResponseObject(user: User): UserOutput {
-        const {token, password, ...returnedUserObject} = user;
-        return returnedUserObject;
-    }
 
     public static getToken(parent: User): string {
         return jwt.sign(JSON.parse(JSON.stringify(parent)), UserHelpers.JWT_SECRET, {
