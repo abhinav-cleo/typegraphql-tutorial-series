73e40d1513022347c4c27228b53d0415014772e3
Gupta <agupta@Cleoadmins-MacBook-Pro.local>
2019-07-05 15:41:13 +0530

    Refactoring

--- Server/src/modules/tradingPartner/TradingPartnerResolver.ts
+++ Server/src/modules/tradingPartner/TradingPartnerResolver.ts
@@ -1,5 +1,5 @@
-import {Resolver, Query, Mutation, Arg, Ctx, Authorized} from "type-graphql";
-import { GraphQLError } from 'graphql';
+import {Arg, Authorized, Mutation, Query, Resolver} from "type-graphql";
+import {GraphQLError} from 'graphql';
 import {TpDetail} from "../../entity/TpDetail";
 import {TradingPartnerInput} from "./TradingPartnerInput";
 
@@ -13,22 +13,27 @@ export class TradingPartnerResolver {
     }
 
     @Authorized()
-    @Query(() => TpDetail, {name: "tradingPartner", nullable: true, description: "Get Trading Partner By Trading Partner " +
-            "Name, Trading Partner Id or external Id"})
+    @Query(() => TpDetail, {
+        name: "tradingPartner", nullable: true, description: "Get Trading Partner By Trading Partner " +
+            "Name, Trading Partner Id or external Id"
+    })
     async getTradingPartner(
         @Arg("identifier") identifier: String
     ): Promise<TpDetail> {
         const numID = +identifier;
-        console.log(`numId ::: ${numID}`);
-        if(!isNaN(numID)){
-            return TpDetail.findOne({where: [
-                    {tpname:identifier},
-                    {tpid:identifier}
-                ]});
+        if (!isNaN(numID)) {
+            return TpDetail.findOne({
+                where: [
+                    {tpname: identifier},
+                    {tpid: identifier}
+                ]
+            });
         } else {
-            return TpDetail.findOne({where: [
-                    {id:identifier}
-                ]});
+            return TpDetail.findOne({
+                where: [
+                    {id: identifier}
+                ]
+            });
         }
 
     }
@@ -39,13 +44,13 @@ export class TradingPartnerResolver {
         @Arg("input") input: TradingPartnerInput
     ): Promise<TradingPartnerInput> {
         const {tpname, id} = input;
-        try{
+        try {
             const tradingPartner = await TpDetail.create({
                 tpname,
                 id
             }).save();
             return tradingPartner;
-        }catch (e) {
+        } catch (e) {
             throw new GraphQLError(e);
         }
     }
@@ -55,20 +60,14 @@ export class TradingPartnerResolver {
         @Arg("identifier") tpid: string,
         @Arg("input") input: TradingPartnerInput
     ): Promise<TradingPartnerInput> {
-        try{
-        console.log(`Trading Partner to be updated ::: ${tpid}`);
-        console.log(`Update Data ::: ${JSON.stringify(input)}`);
-        const tradingPartner = await TpDetail.findOne({where: {tpid}});
-        console.log(`Existing Trading Partner ::: ${JSON.stringify(tradingPartner)}`);
-        if(!tradingPartner){
-            throw new GraphQLError(`Trading partner ${tpid} does not exists`);
-        }
-            const updateResult = await TpDetail.update(tpid,input);
-            console.log(`updateResult ::: ${JSON.stringify(updateResult)}`);
-            const updatedTradingPartner = await TpDetail.findOne({where: {tpid}});
-            return updatedTradingPartner;
-        }catch (e) {
-            console.error(`Error ::: ${JSON.stringify(e)}`);
+        try {
+            const tradingPartner = await TpDetail.findOne({where: {tpid}});
+            if (!tradingPartner) {
+                throw new GraphQLError(`Trading partner ${tpid} does not exists`);
+            }
+            await TpDetail.update(tpid, input);
+            return await TpDetail.findOne({where: {tpid}});
+        } catch (e) {
             throw new GraphQLError(e);
         }
 
